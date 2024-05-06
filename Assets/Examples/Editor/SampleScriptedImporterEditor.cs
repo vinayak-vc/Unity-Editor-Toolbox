097db0e8d14f4c90ead87d2f4e40b30c6ccce7f0
vinayak-vc <vinayak.lakhani@viitor.cloud>
2024-05-06 11:50:01 +0530

    deleted project files

--- Assets/Examples/Editor/SampleScriptedImporterEditor.cs
+++
@@ -1,8 +0,0 @@
-﻿#if UNITY_2020_2_OR_NEWER
-using Toolbox.Editor.Editors;
-using UnityEditor;
-
-[CustomEditor(typeof(SampleScriptedImporter))]
-public class SampleScriptedImporterEditor : ToolboxScriptedImporterEditor
-{ }
-#endif
